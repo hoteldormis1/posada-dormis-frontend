071190297aab563a075ed3d4c15d907c20db0c04
nicovon24 <nicovon24@gmail.com>
2025-06-21 19:26:32 -0300

    :art: fix type reservas

--- app/_models/types/index.ts
+++ app/_models/types/index.ts
@@ -15,11 +15,11 @@ export enum TipoReserva {
 
 export type Reserva = {
   id: string;
-  numeroHab: number;            // Número de habitación reservada
-  ingreso: string;              // Fecha de ingreso (ISO string o Date)
-  egreso: string;               // Fecha de salida (ISO string o Date)
-  huespedNombre: string;        
-  telefonoHuesped: string;      
-  total: number;                // Total a pagar o pagado
-  estadoDeReserva: TipoReserva; 
+  numeroHab: number;
+  ingreso: string;
+  egreso: string;
+  huespedNombre: string;
+  estadoDeReserva: TipoReserva;
+  telefonoHuesped?: string; // ✅ opcional
+  total?: number;           // ✅ opcional
 };
