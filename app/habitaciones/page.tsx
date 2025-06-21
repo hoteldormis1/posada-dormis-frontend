7668ec50e17f5ea51c562b43e4e60cce13d0d300
nicovon24 <nicovon24@gmail.com>
2025-06-21 19:21:49 -0300

    :art: some more styles fixxx

--- app/habitaciones/page.tsx
+++ app/habitaciones/page.tsx
@@ -43,7 +43,7 @@ const Habitaciones = () => {
 	return (
 		<div className={pantallaPrincipalEstilos}>
 			<label className={fuenteDeTitulo}>Habitaciones</label>
-			<div className="w-11/12 sm:w-10/12 md:w-9/12 xl:w-8/12">
+			<div className="w-11/12 sm:w-10/12 md:w-9/12 xl:w-8/12 m-auto">
 				<TableComponent<Room>
 					columns={columns}
 					data={rooms}
